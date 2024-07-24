1d8a7716ea603b0f39db3b7dd9c9396c1463b520
Francescorussoo <164784843+Francescorussoo@users.noreply.github.com>
2024-07-24 21:39:25 +0100

    aggiunta bombe generate

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     const grid = document.getElementById('griglia');
+    const bombCount = 16;
+    const maxCells = 100;
+    const bombs = generateBombs(bombCount, maxCells);
+    let safeClicks = 0;
+
+    console.log('Bombe generate:', bombs); // totale bombe
 
     for (let i = 1; i <= 100; i++) {
         const cell = document.createElement('div');
